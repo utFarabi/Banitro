4ead8b655dd9f0b65f1d33a52504a3cbc3a29281
Mohammad Sianaki <m.rsianaki@yhaoo.com>
2018-10-09 18:09:17 +0330

    batch api key

---
+++ app/src/main/java/com/sm/banitro/BanitroApp.java
@@ -0,0 +1,19 @@
+package com.sm.banitro;
+
+import android.app.Application;
+
+import com.batch.android.Batch;
+import com.batch.android.BatchActivityLifecycleHelper;
+import com.batch.android.Config;
+
+public class BanitroApp extends Application {
+
+    private static final String DEV_API_KEY = "DEV5BBCB8FE2C716C15EBA8A4C51A6";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Batch.setConfig(new Config(DEV_API_KEY));
+        registerActivityLifecycleCallbacks(new BatchActivityLifecycleHelper());
+    }
+}
